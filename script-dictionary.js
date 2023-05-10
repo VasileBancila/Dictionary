a1f7567ff1740128203eb9e2bd0b822bf1fade54
Vasile Bancila <bancilavasile93@gmail.com>
2023-05-10 16:24:39 +0200

    Add new function and adding background

--- script-dictionary.js
+++ script-dictionary.js
@@ -1,23 +1,27 @@
 let dictionary = [];
 
 function addWord() {
-    let word = document.getElementById("word").value;
-    
+    let word = document.getElementById("word").value;    
     dictionary.push(word);
     document.getElementById("word").value = "";
 }
 
 function searchWord() {
     let search = document.getElementById("search");
-    let message = document.getElementById("message");
-    
-    message.innerText = "";
-    
     if (dictionary.includes(search.value)) {
+        messageDisplay("yes");
+    } else {
+        messageDisplay("not")
+    }
+    document.getElementById("search").value = "";
+}
+
+function messageDisplay(status) {
+    let message = document.getElementById("message");
+    message.innerText = "";
+    if (status === "yes") {
         message.innerText = "The word is in the dictionary!";
     } else {
         message.innerText = "The word is not in the dictionary!";
     }
-
-    document.getElementById("search").value = "";
 }
